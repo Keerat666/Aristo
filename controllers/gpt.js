dcd268b6c372ca18b2447fe51d500e739a6223fe
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-10-23 02:21:21 +0530

    added more instructions in the prompt

--- controllers/gpt.js
+++ controllers/gpt.js
@@ -45,7 +45,7 @@ module.exports={
      generatePrompt(code) {
 
 
-        return `Act as a senior developer and simulate a situation where you are reviewing a PR. Try to make only comments that highlight potential bugs and good to have coding style and also tell me how can I resolve that bug and also return me a category for the comment telling me wether that issue is either of the three options in critcality : [High, Medium, Low] then return me a json which has an array of objects in the form , {result : [{fileName,lineNumber,comment,solutionToFixThatComment,category}]}. Please make sure to count the number of lines properly and return the right number. An example array of objects that you can send is like this : 
+        return `Act as a senior developer and simulate a situation where you are reviewing a PR. Try to make comments that highlight potential bugs and also tell me how can I resolve that bug and also return me a category for the comment telling me wether that issue is either of the three options in critcality : [High, Medium, Low] then return me a json which has an array of objects in the form , {result : [{fileName,lineNumber,comment,solutionToFixThatComment,category}]}. If you spot a piece of code that can be written in an optimized manner, please do not hesitate to highlight that and also send a potential solution for it like I explained previously for the bug spotting. Please make sure to count the number of lines properly and return the right number on which you want to comment.Check the code thoroughly for errors including but not limited to : Syntax Errors,Logic Errors,Compilation Errors,Runtime Errors and Arithmetic Errors. An example array of objects that you can send is like this : 
 
           {
             "result": [{fileName : 'filename.js', lineNumber : 3, comment : 'Make sure to take care of other code readability and formatting issues.', solutionToFixThatComment : 'Use proper indentation and formatting to make code readable. Run the file through a linter.', category : 'Low'}]}",
@@ -53,7 +53,7 @@ module.exports={
 
         Point out things like there should not be any code that is not being used in the file. Take care of other code readability and formatting issues. But your main work is to try and spot bugs.
         
-        Only return me a json after doing the code review and nothing else. Please do not return any text other than text than the final array and return the text in a form that if I do JSON.parse then I get a perfectly working json object.
+        Only return me a json after doing the code review and nothing else. Please do not return any text other than text than the final array and return the text in a form that if I do JSON.parse then I get a perfectly working json object of the exact format as specified above.
         
         If you have no comments on the code then return
         
