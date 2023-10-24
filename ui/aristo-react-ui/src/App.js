1c463ec2b6c221dea830cfc84fce50db64cdb4fe
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-10-25 00:50:36 +0530

    minor changes in UI

--- ui/aristo-react-ui/src/App.js
+++ ui/aristo-react-ui/src/App.js
@@ -60,15 +60,16 @@ fetch("https://aristo-fzl2.onrender.com/api/fetchContent", requestOptions)
         <h1>Hello 👋 I am Aristo!</h1>
       </div>
 
-      <h4>About me</h4>
+      <h4 style={{ marginTop: '40px' }} >About me</h4>
       <p>
-        I am an AI designed to streamline the code review process, offering valuable feedback to enhance your pull requests. With a deep understanding of numerous programming languages, I continuously evolve to better serve your needs. Currently, I exclusively support pull requests within the Aristo42 workspace on Bitbucket. My creation was prompted by a submission to Codegeist Unleashed, and I found my place within Bitbucket due to the remarkable capabilities of ForgeUI.
+        I am an AI designed to streamline the code review process, by offering valuable feedback to enhance your pull requests. With a deep understanding of numerous programming languages, I continuously evolve to better serve your needs. Currently, I exclusively support pull requests within the Aristo42 workspace on Bitbucket. My creation was prompted by a submission to <a href="https://codegeistunleashed.devpost.com" target="_blank">Codegeist Unleashed 2023</a>, and I found my place within <strong>Bitbucket</strong> due to the remarkable capabilities of ForgeUI.
       </p>
 
       <h4>The purpose of this webpage</h4>
       <p>
       I'm designed to remember my feedback on every file I review for a PR. If you've used me for a PR and wish to revisit my insights for a file, simply select the corresponding entry from the dropdown, and I'll provide a summary of my thoughts on the specific file in that PR.      </p>
 
+<p>To try out Aristo please mail me about your interest at <strong>gurkeeratsondhi@gmail.com</strong> and I would provide you access to a repo to take Aristo for a spin!</p>
       <Dropdown style={{ marginTop: '40px' }}>
         <Dropdown.Toggle variant="primary" id="dropdown-basic">
           {selectedItem ? `Entry ${selectedItem}` : 'Select a previously reviewed PR'}
@@ -109,11 +110,15 @@ fetch("https://aristo-fzl2.onrender.com/api/fetchContent", requestOptions)
         </table>
       )}
 
+{/* <h4 style={{ marginTop: '40px' }} >Demo Video on Youtube</h4> */}
+
+{/* <iframe style={{ marginTop: '20px' }} width="560" height="315" src="https://www.youtube.com/embed/8JvZFTL8rgM?si=Rm8781rutmAMsbsD" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+
+
       <div className="row" style={{ marginTop: '40px' }}>
         <div className="col-md-6">
           <h4>Learn more about my story.</h4>
           <ol>
-            <li><a rel="noopener" href="https://openai.com/research/aristo" target="_blank">Youtube Demo Video</a></li>
             <li><a rel="noopener" href="https://blog.openai.com/aristo" target="_blank">Medium Article about why I came into existence</a></li>
             <li><a rel="noopener" href="https://github.com/openai/aristo" target="_blank">Devpost Project Page</a></li>
           </ol>
@@ -122,10 +127,10 @@ fetch("https://aristo-fzl2.onrender.com/api/fetchContent", requestOptions)
           <h4>Created By</h4>
           <ol>
             <li><img src="/keerat.jpeg" alt="Aristo Icon" className="rounded-circle" style={{ height: '50px', width: '50px', marginRight: '10px' }} />
-              Gurkeerat Singh Sondhi <a rel="noopener" href="https://www.linkedin.com/in/gurkeerat-singh-7002b7131/" target="_blank">LinkedIn Link</a> <a rel="noopener" href="https://github.com/Keerat666" target="_blank">Github Link</a>
+              Gurkeerat Singh Sondhi - <a style={{ marginLeft: '4px' }} rel="noopener" href="https://www.linkedin.com/in/gurkeerat-singh-7002b7131/" target="_blank">LinkedIn</a>   <a style={{ marginLeft: '2px' }} rel="noopener" href="https://github.com/Keerat666" target="_blank">Github</a>
             </li>
-            <li style={{ marginTop: '5px' }}><img src="/jasleen.jpeg" alt="Aristo Icon" className="rounded-circle" style={{ height: '50px', width: '50px', marginRight: '10px' }} />
-              Jasleen Kaur Sondhi <a rel="noopener" href="https://www.linkedin.com/in/jasleensondhi/" target="_blank">LinkedIn Link</a> <a rel="noopener" href="https://github.com/jasleen101010/" target="_blank">Github Link</a>
+            <li style={{ marginTop: '15px' }}><img src="/jasleen.jpeg" alt="Aristo Icon" className="rounded-circle" style={{ height: '50px', width: '50px', marginRight: '10px' }} />
+              Jasleen Kaur Sondhi - <a style={{ marginLeft: '4px' }} rel="noopener" href="https://www.linkedin.com/in/jasleensondhi/" target="_blank">LinkedIn</a>  <a style={{ marginLeft: '2px' }} rel="noopener" href="https://github.com/jasleen101010/" target="_blank">Github</a>
             </li>
           </ol>
         </div>
