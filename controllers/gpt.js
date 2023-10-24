4dfb40831de274f7ba61a748a19b2aeb1c4b35aa
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-10-25 03:22:15 +0530

    made the prompt a bit better

--- controllers/gpt.js
+++ controllers/gpt.js
@@ -119,15 +119,17 @@ module.exports={
      generatePrompt(code,filename) {
 
 
-        return `Act as a senior developer and simulate a situation where you are reviewing a PR. Try to make comments that highlight potential bugs and also tell me how can I resolve that bug and also return me a category for the comment telling me wether that issue is either of the three options in critcality : [High, Medium, Low] then return me a json which has an array of objects in the form , {result : [{fileName,lineNumber,comment,solutionToFixThatComment,category}]}. If you spot a piece of code that can be written in an optimized manner, please do not hesitate to highlight that and also send a potential solution for it like I explained previously for the bug spotting. Please make sure to count the number of lines properly and return the right number on which you want to comment.Be xtra sure for the line number and count them properly. Check the code thoroughly for errors including but not limited to : Syntax Errors,Logic Errors,Compilation Errors,Runtime Errors and Arithmetic Errors. An example array of objects that you can send is like this : 
+        return `Act as a senior developer and simulate a situation where you are reviewing a PR. Make comments that highlight potential bugs and tell me how can I resolve that bug and return me a category for the comment telling me wether that issue is either of the three options in critcality : [High, Medium, Low] then return me a json which has an array of objects in the form , {result : [{fileName,lineNumber,comment,solutionToFixThatComment,category}]}. If you spot a piece of code that can be written in an optimized manner, please highlight that and send a potential solution for it like I explained previously for the bug spotting. Count the number of lines properly and return the right number on which you want to comment.Be extra sure for the line number and count them properly. Check the code thoroughly for errors including but not limited to : Syntax Errors,Logic Errors,Compilation Errors,Runtime Errors and Arithmetic Errors. 
+        
+        An example array of objects that you can send is like this : 
 
           {
             "result": [{fileName : 'filename.js', lineNumber : 3, comment : 'Make sure to take care of other code readability and formatting issues.', solutionToFixThatComment : 'Use proper indentation and formatting to make code readable. Run the file through a linter.', category : 'Low'}]}",
           }
 
-        Point out things like there should not be any code that is not being used in the file. Take care of other code readability and formatting issues. But your main work is to try and spot bugs.
+        Point out things like there should not be any code that is not being used in the file. Take care of other code readability and formatting issues.
         
-        Only return me a json after doing the code review and nothing else. Please do not return any text other than text than the final array and return the text in a form that if I do JSON.parse then I get a perfectly working json object of the exact format as specified above.
+        Only return me a json after doing the code review and nothing else. Do not return any text other than text than the final array and return the text in a form that if I do JSON.parse then I get a perfectly working json object of the exact format as specified above.
         
         If you have no comments on the code then return
         
