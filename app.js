55cd7ab3a04d94de508fa112c04b04f1c52a2ec2
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-10-25 00:14:40 +0530

    * bug solved

--- app.js
+++ app.js
@@ -11,16 +11,6 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'ui/aristo-react-ui/build')));
-
-// Handle other routes and return the React app
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'ui/aristo-react-ui/build/index.html'));
-});
-
-
 //Please don't delete this health API
 app.use('/api/health', (req, res) => {
     res.send('Hello Aristo!');
@@ -44,6 +34,14 @@ app.use('/api/health', (req, res) => {
     gpt.fetchContentOfFile(req,res)
 
   })
+
+  // Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'ui/aristo-react-ui/build')));
+
+// Handle other routes and return the React app
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ui/aristo-react-ui/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
